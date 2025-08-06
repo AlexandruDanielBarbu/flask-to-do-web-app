877a8a7c882121e1d26eb6c23966c004a4211337
Barbu Alexandru Daniel <barbualex.daniel2004@gmail.com>
2025-08-06 14:21:29 +0300

    Added a jinja template and a database
    Signed-off-by: Barbu Alexandru Daniel <barbualex.daniel2004@gmail.com>

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 # Imports
-from flask import Flask
+from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # My App
 app = Flask(__name__)
+Scss(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasks_database.db"
+db = SQLAlchemy(app)
+
+class MyTask(db.Model):
+    id          = db.Column(db.Integer, primary_key=True)
+    content     = db.Column(db.String(100), nullable=False)
+    completed   = db.Column(db.Integer, default=0)
+    created     = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Task {self.id}"
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
 if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
