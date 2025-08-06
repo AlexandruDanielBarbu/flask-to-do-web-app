b7784176e5438f1590db83f5fa7a6108cbc31d29
Barbu Alexandru Daniel <barbualex.daniel2004@gmail.com>
2025-08-06 11:53:37 +0300

    Working test of my tutorial app
    Signed-off-by: Barbu Alexandru Daniel <barbualex.daniel2004@gmail.com>

---
+++ app.py
@@ -0,0 +1,14 @@
+# Imports
+from flask import Flask
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+
+# My App
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+if __name__ in "__main__":
+    app.run(debug=True)
