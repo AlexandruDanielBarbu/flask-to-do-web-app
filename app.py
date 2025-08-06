0c94023eb8c585a70f17e8515b174916c4851e4f
Barbu Alexandru Daniel <barbualex.daniel2004@gmail.com>
2025-08-06 14:52:54 +0300

    User can now delete the tasks
    Signed-off-by: Barbu Alexandru Daniel <barbualex.daniel2004@gmail.com>

--- app.py
+++ app.py
@@ -39,6 +39,19 @@ def index():
         tasks = MyTask.query.order_by(MyTask.created).all()
         return render_template("index.html", tasks=tasks)
 
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        print(f"ERROR {e}")
+        return f"ERROR {e}"
+
+
 if __name__ in "__main__":
     with app.app_context():
         db.create_all()
